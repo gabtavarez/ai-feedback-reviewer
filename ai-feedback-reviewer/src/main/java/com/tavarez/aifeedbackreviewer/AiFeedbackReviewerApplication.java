d8a594d483e9b71bb51cc75a93f62232ca5428ac
gabtavarez <gabrieltavarescs03@gmail.com>
2026-04-27 18:57:13 -0300

    first commit

---
+++ ai-feedback-reviewer/src/main/java/com/tavarez/aifeedbackreviewer/AiFeedbackReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.tavarez.aifeedbackreviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFeedbackReviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiFeedbackReviewerApplication.class, args);
+    }
+
+}
---
+++ ai-feedback-reviewer/src/test/java/com/tavarez/aifeedbackreviewer/AiFeedbackReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tavarez.aifeedbackreviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFeedbackReviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
